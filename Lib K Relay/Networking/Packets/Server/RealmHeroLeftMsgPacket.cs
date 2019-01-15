9d6b8337c1b0ee91d428b2331b1b6689e5185c3f
kibaya <46716532+kibay@users.noreply.github.com>
2019-01-15 23:01:43 +0300

    Add RealmHeroLeftMsg packet and update xml files

---
+++ Lib K Relay/Networking/Packets/Server/RealmHeroLeftMsgPacket.cs
@@ -0,0 +1,20 @@
+﻿namespace Lib_K_Relay.Networking.Packets.Server
+{
+    public class RealmHeroLeftMsgPacket : Packet
+    {
+        public int RealmHeroesLeft;
+
+        public override PacketType Type
+        { get { return PacketType.REALMHEROLEFTMSG; } }
+
+        public override void Read(PacketReader r)
+        {
+            RealmHeroesLeft = r.ReadInt32();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(RealmHeroesLeft);
+        }
+    }
+}
